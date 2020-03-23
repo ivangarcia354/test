d9ef3dc2f104ab7f0e9f8c89261510f237ef21bc
ivan garcia <ivangarciatech@gmail.com>
2020-03-23 17:49:30 -0300

    otro cambio2

--- faunadb-demo/pages/api/index.js
+++ faunadb-demo/pages/api/index.js
@@ -14,7 +14,7 @@ module.exports = async (req, res) => {
           // make paginatable
           q.Match(
             // query index
-            q.Index('all_customers') // specify source
+            q.Index('all_products') // specify source
           )
         ),
         ref => q.Get(ref) // lookup each result by its reference
