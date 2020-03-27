a9d20a54d9e6873a9da88607275335e9577a7ea9
ivangarcia354 <62561608+ivangarcia354@users.noreply.github.com>
2020-03-26 23:02:55 -0300

    Update index.js

--- faunadb-demo/pages/index.js
+++ faunadb-demo/pages/index.js
@@ -2,15 +2,6 @@ import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import TableRow from '../components/TableRow'
 
-class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [], text: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-
 export default () => {
   const [data, setData] = useState([])
   useEffect(() => {
@@ -38,24 +29,6 @@ export default () => {
         </form>
       </div>
     );
-};
-
- handleChange(e) {
-    this.setState({ text: e.target.value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.text.length === 0) {
-      return;
-    }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
-  }
 }
+
+ 
